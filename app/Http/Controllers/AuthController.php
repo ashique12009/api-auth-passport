8074bdc00f08563d614109700a9857afc842ffc7
Ashique <ashique12009@gmail.com>
2021-09-08 21:25:36 +0600

    wrapped up login function with try block

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,19 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Auth;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller {
 
   public function login(Request $request) {
-    if (Auth::attempt($request->only('email', 'password'))) {
-      $user = Auth::user();
-      return $user;
-    }
+    try {
+      if (Auth::attempt($request->only('email', 'password'))) {
+        $user  = Auth::user();
+        $token = $user->createToken('app')->accessToken;
+  
+        return response([
+          'message' => 'success',
+          'token'   => $token,
+          'user'    => $user,
+        ]);
+      }
+    } catch (\Exception $exception) {
+      return response([
+        'message' => $exception->getMessage()
+      ], 400);
+    }    
 
     return response([
-      'message' => 'Invalid username/password'
+      'message' => 'Invalid username/password',
     ], 401);
   }
 
